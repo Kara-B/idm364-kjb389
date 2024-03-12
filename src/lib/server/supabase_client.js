6950ce872a361ba3f25ab81fae248accf0f624f8
Kara-B <kjbutler819@verizon.net>
2024-03-12 14:52:17 -0400

    client test

--- src/lib/server/supabase_client.js
+++ src/lib/server/supabase_client.js
@@ -1,5 +1,5 @@
-import { VITE_PUBLIC_SUPABASE_ANON_KEY, VITE_PUBLIC_SUPABASE_URL } from '$env/static/public';
+// import { VITE_PUBLIC_SUPABASE_ANON_KEY, VITE_PUBLIC_SUPABASE_URL } from '$env/static/public';
 
-import { createClient } from '@supabase/supabase-js';
+// import { createClient } from '@supabase/supabase-js';
 
-export const supabase = createClient(VITE_PUBLIC_SUPABASE_URL, VITE_PUBLIC_SUPABASE_ANON_KEY);
+// export const supabase = createClient(VITE_PUBLIC_SUPABASE_URL, VITE_PUBLIC_SUPABASE_ANON_KEY);t
