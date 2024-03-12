a649cb164100908340b00add182818e4bfca7e1b
Kara-B <kjbutler819@verizon.net>
2024-03-12 00:49:26 -0400

    Path Updated
    again

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -3,7 +3,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 // Use `import.meta.env` to access environment variables
-const supabaseUrl = import.meta.env.PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.PUBLIC_SUPABASE_ANON_KEY;
+const supabaseUrl = import.meta.env.VITE_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_PUBLIC_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
